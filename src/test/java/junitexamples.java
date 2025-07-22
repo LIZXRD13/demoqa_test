cd919b7d8780547b00299743e3b7995bd4795612
LIZXRD13 <rezaev1313@gmail.com>
2025-07-22 21:08:38 +0300

    added some tests

--- src/test/java/junitexamples.java
+++ src/test/java/junitexamples.java
@@ -1,18 +1,20 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class junitexamples {
 
-  @BeforeAll
-  static void browserConfig(){
-    Configuration.browserSize = "2560x1440";
+  /*@BeforeAll
+  static void browserConfig() {
+    Configuration.browserSize = "1920x1080";
   }
+
     @Test
       void test() {
         open("https://demoqa.com/text-box");
@@ -23,6 +25,19 @@ public class junitexamples {
         $("#submit").click();
         $("#output").shouldHave(text("Name:Charles Leclerc"), text("Email:leclerc@f1.com"), text("Current Address :UK, Silverstone"),
                 text("Permananet Address :Monaco, Nabergnaya u morya, 9"));
-      }
+      }*/
+  @Test
+  void test() {
+    open("https://github.com/");
+    $("[placeholder='Search or jump to...']").click();
+    $("#query-builder-test").setValue("Selenide").pressEnter();
+    $("[class='Box-sc-g0xbh4-0 kzfhBO search-match prc-Text-Text-0ima0']").click();
+    $("#repository-container-header").shouldHave(text("selenide"));
+    $("div.Layout-sidebar").$(byText("Contributors"));
 
+    sleep(5000);
+
+
+
+  }
 }
